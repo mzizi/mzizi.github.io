c2f0f4734998e58a611ccb7ceb83e891a90e3223
Rodgers Gitau <rodgersgitau@gmail.com>
2023-11-02 22:59:28 +0300

    chore: update astrowind template

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,32 +1,37 @@
-import { BLOG, SITE } from '~/config.mjs';
-// import { fetchPosts } from '~/utils/blog';
+import { getRssString } from '@astrojs/rss';
+
+import { SITE, METADATA, APP_BLOG } from '~/utils/config';
+import { fetchPosts } from '~/utils/blog';
 import { getPermalink } from '~/utils/permalinks';
 
-import rss from '@astrojs/rss';
-
-export const get = async () => {
-  if (BLOG.disabled) {
+export const GET = async () => {
+  if (!APP_BLOG.isEnabled) {
     return new Response(null, {
       status: 404,
       statusText: 'Not found',
     });
   }
 
-  // const posts = await fetchPosts();
+  const posts = await fetchPosts();
 
-  return rss({
+  const rss = await getRssString({
     title: `${SITE.name}’s Blog`,
-    description: SITE.description,
+    description: METADATA?.description || '',
     site: import.meta.env.SITE,
-    items: [],
 
-    // items: posts.map((post) => ({
-    //   link: getPermalink(post.permalink, 'post'),
-    //   title: post.title,
-    //   description: post.description,
-    //   pubDate: post.publishDate,
-    // })),
+    items: posts.map((post) => ({
+      link: getPermalink(post.permalink, 'post'),
+      title: post.title,
+      description: post.excerpt,
+      pubDate: post.publishDate,
+    })),
 
-    trailingSlash: SITE.trailingSlash
+    trailingSlash: SITE.trailingSlash,
+  });
+
+  return new Response(rss, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
   });
 };
